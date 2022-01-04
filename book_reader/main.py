e153d45e7a38458ffac80788c936f39dba2f2ab7
kakakoi <56661650+kakakoi@users.noreply.github.com>
2022-01-04 23:09:56 +0900

    2page mode

--- book_reader/main.py
+++ book_reader/main.py
@@ -3,6 +3,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty, ListProperty, NumericProperty
 
+from kivy.core.window import Window
+
 import rarfile
 import os
 import shutil
@@ -11,12 +13,16 @@ import glob
 def listup_files(path):
     yield [os.path.abspath(p) for p in glob.glob(path)]
 
+
 class ImageWidget(Widget):
-    source: StringProperty
+    source_main: StringProperty
+    source_sub: StringProperty
     source_files: ListProperty
     source_index_max : NumericProperty
+    view_num : NumericProperty = 2
     source_index : NumericProperty = 0
-    rf = rarfile.RarFile(".rar")
+    file_path = ""
+    rf = rarfile.RarFile(file_path)
     shutil.rmtree('tmp')
     rf.extractall("./tmp")
     files = listup_files('tmp/**/*.jpg')
@@ -25,24 +31,40 @@ class ImageWidget(Widget):
             files = f
     if len(files) > 0:
         source_files = sorted(files)
-        source = StringProperty(source_files[0])
+        source_main = StringProperty(source_files[0])
+        source_sub = StringProperty(source_files[1])
         source_index_max = len(source_files) -1
 
     def __init__(self, **kwargs):
         super(ImageWidget, self).__init__(**kwargs)
-        pass
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
-    def buttonNext(self):
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'left':
+            self.next()
+        elif keycode[1] == 'right':
+            self.prev()
+        return True
+
+    def next(self):
         if self.source_index < self.source_index_max:
-            self.source_index += 1
-        self.source = self.source_files[self.source_index]
-        print(self.source)
+            self.source_index += self.view_num
+        self.source_main = self.source_files[self.source_index]
+        if self.source_index+1 < self.source_index_max:
+            self.source_sub = self.source_files[self.source_index+1]
+        print(self.source_main)
 
-    def buttonPrev(self):
+    def prev(self):
         if self.source_index > 0:
-            self.source_index -= 1
-        self.source = self.source_files[self.source_index]
-        print(self.source)
+            self.source_index -= self.view_num
+        self.source_main = self.source_files[self.source_index]
+        self.source_sub = self.source_files[self.source_index+1]
+        print(self.source_main)
 
 class ReaderApp(App):
     def __init__(self, **kwargs):
